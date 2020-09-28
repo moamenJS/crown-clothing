d58a8d5451939f154a484086f811a012dc10e021
moamenJS <moamenmedleg@hotmail.com>
2020-09-28 03:42:43 +0300

    finished styling the header

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,14 +3,15 @@ import React from "react";
 import "./menu-item.styles.scss";
 
 const MenuItem = (props) => (
-  <div
-    style={{
-      backgroundImage: `URL(${props.imageUrl})`,
-    }}
-    className={`${props.size} menu-item`}
-  >
+  <div className={`${props.size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `URL(${props.imageUrl})`,
+      }}
+    />
     <div className="content">
-      <h1 className="title">{props.title}</h1>
+      <h1 className="title">{props.title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
