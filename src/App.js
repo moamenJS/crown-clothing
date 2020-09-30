0f0703fbae4985aabdcbf73252af3c3e595b283a
moamenJS <moamenmedleg@hotmail.com>
2020-09-30 15:38:58 +0300

    updating our app with a shop page. collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import { HomePage } from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 //import { HomePage } from "./pages/homepage/homepage.component";
 
 const HatsPage = (props) => {
@@ -20,6 +21,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 
+import "./shop.styles.scss";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 class ShopPage extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      collections: SHOP_DATA,
+    };
   }
   render() {
     return (
       <div>
-        <div></div>
+        {this.state.collections.map(({ id, ...iterator }) => (
+          <CollectionPreview key={id} {...iterator} />
+        ))}
       </div>
     );
   }
