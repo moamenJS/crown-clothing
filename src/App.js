378dd8e44c77627c4333cfecaa4e7b47af0945d0
moamenJS <moamenmedleg@hotmail.com>
2020-09-30 20:32:18 +0300

    adding header component with SHOP, LOGO and CONTACT US navigation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
+import Header from "./components/header/header.component";
 import { HomePage } from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-//import { HomePage } from "./pages/homepage/homepage.component";
-
+//import Header from "./components/header/header.component";
 const HatsPage = (props) => {
   return (
     <div>
@@ -18,6 +18,7 @@ const App = () => {
   // return <HomePage />;
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/hats" component={HatsPage} />
